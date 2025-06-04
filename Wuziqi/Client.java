f87d211e8569d3641c22bca72fe60719c3381426
神必仓库 <145906118+g1gg123@users.noreply.github.com>
2025-06-04 10:23:29 +0800

    Update Client.java

--- Wuziqi/Client.java
+++ Wuziqi/Client.java
@@ -22,7 +22,7 @@ public class Client{
 
     public Client(String host, int port){
         try{
-            Socket socket=new Socket(host,port);
+            socket=new Socket(host,port);
             in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         }
